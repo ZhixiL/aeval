276e3d2a3298bd3cd72cefc8cd6aeebd5e440359
ZhixiL <zl19@my.fsu.edu>
2021-07-15 08:58:21 -0400

    initial migration of skolem generation code & configured to be compilable, will further improve

--- include/ae/AeValSolver.hpp
+++ include/ae/AeValSolver.hpp
@@ -13,6 +13,11 @@ namespace ufo
   /** engine to solve validity of \forall-\exists formulas and synthesize Skolem relation */
 
   Expr mixQE(Expr s, Expr constVar, ExprMap &substsMap, ZSolver<EZ3>::Model &m);
+  Expr static createQuantifiedFormulaRestr (Expr def, Expr a, bool forall);
+  Expr multTrans(Expr t, Expr constY);
+  Expr revExpr(Expr s);
+  Expr negativeCoefCheck(Expr t);
+  Expr vecElemInit(Expr t, Expr constY);
 
   class AeValSolver {
   private:
@@ -805,6 +810,8 @@ namespace ufo
 
     Expr compositeAssm(ExprSet& cnjs, Expr var, bool isInt)
     {
+      outs() << "Var: " << var << endl;
+      outs() << "cnjs: " << conjoin(cnjs, efac) << endl;
         bool incomplete = false;
 
         // split constraints
@@ -817,6 +824,116 @@ namespace ufo
         ExprSet conjEQ;
 
         u.removeRedundantConjuncts(cnjs);
+        
+
+        Expr s = conjoin(cnjs, efac);
+        outs() << "Printing original SMT formula:\n" << *s << endl << endl;
+        s = simplifyBool(s);
+        ExprSet E, D, G, LE, temp; //Use Expr vector instead
+        getConj(s, temp);
+        Expr constOne = mkTerm(mpz_class(1), s->getFactory());
+        Expr constYTemp = mkTerm <std::string> ("y", s->getFactory());
+        Expr constY = bind::intConst(constYTemp);
+
+        ExprVector sVec;
+        //initializing Expression Vector, ensure y is not on rhs, remove LT & GEQ.
+        for (auto t : temp) sVec.push_back(vecElemInit(t, constY));
+
+        while (true)
+        {
+          ExprVector sVecTemp;
+          auto locIte = sVec.begin();
+          while (locIte != sVec.end())
+          {
+            Expr lhs = (*locIte) -> left(), rhs = (*locIte) -> right(), curExp = (*locIte);
+            if (!contains(curExp, constY)) 
+            {
+              outs() << "ERROR: Y is not on LHS of " << *curExp << "on the start." << endl;
+              exit(0);
+            } else if (curExp->arity() != 2)
+            {
+              outs() << "ERROR: The expression " << *curExp << "is invalid!" << endl;
+            }
+            if (lhs->arity() != 1)
+            {
+              //MULTIPLICATION TRANSFORMATION
+              if (isOp<MULT>(lhs)) sVecTemp.push_back(multTrans(curExp, constY));
+
+              //DIVISION TRANSFORMATION
+              else if (isOp<DIV>(lhs))
+              {
+                Expr lhs = curExp->left(), rhs = curExp->right();
+                Expr alpha = lhs->right(), varY = lhs->left();
+                if (!contains(varY, constY)) 
+                {
+                  outs() << "ERROR on divTrans, f(x)/y format not supported." << endl;
+                  exit(0);
+                }
+
+                //applying section 4.2, divisibility constraints
+                if (isOpX<EQ>(curExp)){
+                  sVecTemp.push_back(mk<GT>(varY, mk<MINUS>(mk<MULT>(alpha, rhs), constOne)));
+                  sVecTemp.push_back(mk<LEQ>(varY, mk<MINUS>(mk<PLUS>(mk<MULT>(alpha, rhs), alpha), constOne)));
+                } else if (isOpX<GT>(curExp)){
+                  sVecTemp.push_back(mk<GT>(varY, mk<MINUS>(mk<PLUS>(mk<MULT>(alpha, rhs), alpha), constOne)));
+                } else if (isOpX<LEQ>(curExp)){
+                  sVecTemp.push_back(mk<LEQ>(varY, mk<MINUS>(mk<PLUS>(mk<MULT>(alpha, rhs), alpha), constOne)));
+                } else if (isOpX<NEQ>(curExp)){
+                  int i = -1;
+                  if (isOpX<MPZ>(alpha)) i = lexical_cast<int>(*(alpha)) - 1;
+                  else outs() << "Issue with NEQ expression, no alpha found." << *curExp << endl;
+                  while (i >= 0)
+                  {
+                    sVecTemp.push_back(mk<NEQ>(varY, i != 0 ? //Ensure 0 is not added to expression
+                      mk<PLUS>(mk<MULT>(alpha, rhs), mkTerm(mpz_class(i), s->getFactory())) :
+                      mk<MULT>(alpha, rhs)));
+                    --i;
+                  }
+                }
+              } else outs() << "The current operation on LHS : " << *lhs << " is not supported!" << endl;
+              sVec.erase(locIte);
+              break;
+            }
+            else locIte += 1;
+          }
+          // no change detected, break while loop.
+          if (sVecTemp.empty()) break; 
+          // Merging sVecTemp w/ sVec
+          else for (auto ite = sVecTemp.begin(); ite != sVecTemp.end(); ++ite) sVec.push_back(*ite);
+        }
+
+        //DIVIDE EXPRESSION INTO DIFFERENT CATEGORY
+        for (auto ite = sVec.begin(); ite != sVec.end(); ){
+          if (isOpX<NEQ>(*ite))  D.insert(*ite);
+          else if (isOpX<EQ>(*ite)) E.insert(*ite);
+          else if (isOpX<GT>(*ite)) G.insert(*ite);
+          else if (isOpX<LEQ>(*ite)) LE.insert(*ite);
+          else if (isOpX<GEQ>(*ite)) outs() << "Error, GEQ detected: " << *ite << endl;
+          else if (isOpX<LT>(*ite)) outs() << "Error, LT detected: " << *ite << endl;
+          else outs() << "Insertion ERROR\n";
+          ite++;
+        }
+
+        //PRINTING SECTION
+        outs() << "Following expressions divided by comparison type:\nE: ";
+        for (auto t : E) outs() <<  *t  << " ";
+        outs() << "\nD: ";
+        for (auto t : D) outs() <<  *t  << " ";
+        outs() << "\nG: ";
+        for (auto t : G) outs() <<  *t  << " ";
+        outs() << "\nLE: ";
+        for (auto t : LE) outs() <<  *t  << " ";
+        outs() << endl;
+        
+        
+        ExprSet final;
+        for(auto t : E) final.insert(t);
+        for(auto t : D) final.insert(t);
+        for(auto t : G) final.insert(t);
+        for(auto t : LE) final.insert(t);
+        SMTUtils u1(s->getFactory());
+        outs() << "Is the end equivalent to the beginning: " << u1.isEquiv(s, conjoin(final, s->getFactory())) << endl;
+        cnjs = final;
 
         for (auto cnj : cnjs)
         {
@@ -946,7 +1063,6 @@ namespace ufo
         if (conjEQ.size() > 0) return *(conjEQ.begin()); // GF: maybe try to find the best of them
 
         // get symbolic max and min
-
         Expr curMaxGT, curMaxGE, curMinLT, curMinLE, curMax, curMin;
         getSymbolicMax(conjGT, curMaxGT, isInt);
         getSymbolicMax(conjGE, curMaxGE, isInt);
@@ -985,6 +1101,18 @@ namespace ufo
             else curMin = mk<ITE>(mk<LEQ>(curMinLT, curMinLE), curMinLT, curMinLE);
           }
         }
+        outs() << "curMaxGT: " << curMaxGT << "\n";
+        outs() << "curMaxGE: " << curMaxGE << "\n";
+        outs() << "curMinLT: " << curMinLT << "\n";
+        outs() << "curMinLE: " << curMinLE << "\n";
+        outs() << "curMax: " << curMax << "\n";
+        outs() << "curMin: " << curMin << "\n";
+        outs() << "conjLT: " << conjoin(conjLT, efac) << endl;
+        outs() << "conjLE: " << conjoin(conjLE, efac) << endl;
+        outs() << "conjGT: " << conjoin(conjGT, efac) << endl;
+        outs() << "conjGE: " << conjoin(conjGE, efac) << endl;
+        outs() << "conjNEQ: " << conjoin(conjNEQ, efac) << endl;
+        outs() << "conjEQ: " << conjoin(conjEQ, efac) << endl;
 
         if (conjNEQ.size() == 0)
         {
@@ -1471,6 +1599,30 @@ namespace ufo
       return conjoin(constrs, efac);
     }
   };
+
+  /* OLD HELPER FUNCTIONS */
+  //used when initializing the element for sVec, most basic initializer.
+    Expr vecElemInit(Expr t, Expr constY)
+    {
+      if (t->arity() == 2)
+      {
+        //ensure y is on lhs.
+        if (contains(t->right(), constY)) t = revExpr(t);
+        Expr lhs = t->left(), rhs = t->right(), constOne = mkTerm(mpz_class(1), t->getFactory());
+        //ensure lhs is not negative
+        if (isOpX<UN_MINUS>(lhs->left()) || isOpX<UN_MINUS>(lhs->right())) {
+          t = negativeCoefCheck(t);
+          lhs = t->left(), rhs = t->right();
+        }
+        //applying (3), getting rid of LT and GEQ
+        if (isOpX<LT>(t)) t = mk<LEQ>(lhs, mk<MINUS>(rhs, constOne));
+        else if (isOpX<GEQ>(t)) t = mk<GT>(lhs, mk<MINUS>(rhs, constOne));
+        return t;
+      } else {
+        outs() << "ERROR on invoking vecElemInit! The input Expr t is invalid!" << endl;
+        exit(0);
+      }
+    }
 
   /* GIVEN HELPER FUNCTION */
   // create forall & exists formulas
